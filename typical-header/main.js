0e9dd1d22a1edb0339a0371a5b9e0303fa5e1128
modivicious <dens.2001@bk.ru>
2022-03-05 03:07:03 +0300

    add toggleMenu function

--- typical-header/main.js
+++ typical-header/main.js
@@ -1,3 +1,34 @@
+const menuList = document.querySelector(".menu__list");
+const menuOverlay = document.querySelector(".menu .overlay");
+const menuBtn = document.querySelector(".menu__btn");
+const menuLogin = document.querySelector(".menu__login");
+
+function toggleMenu() {
+  menuList.classList.toggle("menu__list--active");
+  menuOverlay.classList.toggle("menu__overlay--active");
+  menuBtn.classList.toggle("menu__btn--active");
+  menuLogin.classList.toggle("menu__login--active");
+  document.body.classList.toggle("hide-overflow");
+}
+
+menuBtn.on("click", () => toggleMenu());
+menuOverlay.on("click", () => toggleMenu());
+
+
+// JQ:
+
+// function toggleMenu() {
+//   $(".menu__list").toggleClass("menu__list--active");
+//   $(".menu__overlay").toggleClass("menu__overlay--active");
+//   $(".menu__btn").toggleClass("menu__btn--active");
+//   $(".menu__login").toggleClass("menu__login--active");
+//   $("body").toggleClass("hide-overflow");
+// }
+
+// $(".menu__btn").on("click", () => toggleMenu());
+// $(".menu__overlay").on("click", () => toggleMenu());
+
+
 let lastScroll = 0;
 const header = document.querySelector(".header");
 
